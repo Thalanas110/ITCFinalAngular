c88ac5ff5bd766112dc89083e0324a316bd651e5
Thalanas110 <120995590+Thalanas110@users.noreply.github.com>
2024-11-03 01:43:37 +0000

    routes fixed

--- maamhaidee/src/app/app.routes.ts
+++ maamhaidee/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { IntroComponent } from './intro/intro.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ServicesComponent } from './services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = 
+[
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'intro', component: IntroComponent },
+    { path: 'portfolio', component: PortfolioComponent },
+    { path: 'services', component: ServicesComponent }
+];
